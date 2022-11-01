058c3edd37374064b9c4a210d0f8d8aab020cbdf
Alonso Garrido <89758367+arizonv@users.noreply.github.com>
2022-11-01 18:07:45 +0100

    ok

--- frontend/huston/crud/views.py
+++ frontend/huston/crud/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+import requests
+import json
 
-# Create your views here.
+
+url_api = "http://127.0.0.1:8000/api/developer/"
+
+def getPersona(request):
+
+    response = requests.get(url_api).json()
+    datos = {'personas': response}
+    return render('home.html', datos)
+
+def gerPrueba(request):
+
+    url_api = "http://127.0.0.1:8000/api/developer/"
+
+    response = requests.get(url_api).json()
+    datos = {'persona': response}
+
+    return render(request, 'home.html', datos)
